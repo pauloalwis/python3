a27df48bf32e8b8a5716bb41eafaace821069982
pauloalwis <pauloalwis@gmail.com>
2018-06-03 21:21:08 -0300

    update from dev

--- leia.um.numero.inteiro.e.mostre.sua.tabuada.py
+++ leia.um.numero.inteiro.e.mostre.sua.tabuada.py
@@ -1,4 +1,8 @@
 numero = int(input('Digite um número para ser apresentado sua tabuada!'))
 
+print(20 * '=')
 for i in range(0, 11):
-    print(numero, ' x ', i, ' = ', numero * i)
+    print('{} x {:2} = {}'.format(numero, i, (numero * i)))
+
+print(20 * '=')
+
