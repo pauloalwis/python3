957673be5d6c2c8f6b6a5078ad52cd84bbe50bb2
pauloalwis <pauloalwis@gmail.com>
2018-06-23 12:47:56 -0300

    update from dev

--- funcao_recursiva.py
+++ funcao_recursiva.py
@@ -1,3 +1,4 @@
+# Exemplo 01
 # O fatorial de um número corresponde ao produto desse número
 # pelo fatorial de seu número antecessor.
 
@@ -12,3 +13,13 @@ x = int(input("Digite um número para calcular seu fatorial: "))
 res = factorial(x)
 
 print("O fatorial de {} é {}".format(x ,res))
+
+# Exemplo 02
+# A sequência de Fibonacci também pode ser obtida a partir
+# de uma função recursiva.
+
+# Ela começa com os números 0 e 1, e os valores seguinte são a
+# soma dos anteriores a ele, como seque:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,...
+
